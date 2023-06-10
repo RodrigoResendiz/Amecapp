c94751b8b871d224fa1b13141bd80c1c8ba9670c
RODRIGO RESENDIZ <rodrigoresendiz@outlook.es>
2023-06-10 13:07:02 -0600

    Initial commit

---
+++ app/src/main/java/com/gobernanza/pruebalogin/MainActivity.java
@@ -0,0 +1,82 @@
+package com.gobernanza.pruebalogin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText Correo, contraseña;
+    Button Login;
+    TextView registro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        setTitle("Login app");
+        Correo = findViewById(R.id.editTexEmail);
+        contraseña = findViewById(R.id.editTextPassword);
+        Login = findViewById(R.id.buttonLogin);
+        registro = findViewById(R.id.textViewRegistro);
+
+        Login.setOnClickListener(this);
+        registro.setOnClickListener(this);
+
+
+    }
+        @Override
+        public void onClick (View v) {
+            int id = v.getId();
+            switch (id) {
+                case R.id.buttonLogin:
+                    if (!Correo.getText().toString().isEmpty() && !contraseña.getText().toString().isEmpty()) {
+                        FirebaseAuth.getInstance().signInWithEmailAndPassword(Correo.getText().toString(), contraseña.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    GoToHome(task.getResult().getUser().getEmail() != null ? task.getResult().getUser().getEmail() : "");
+                                } else {
+                                    showAlert();
+                                }
+                            }
+                        });
+                    }
+                    break;
+                case R.id.textViewRegistro:
+                    GoToRegistro();
+            }
+
+        }
+
+    private void GoToRegistro() {
+    Intent i = new Intent(this,Registro.class);
+    startActivity(i);
+    finish();
+    }
+
+    private void showAlert() {
+        Toast.makeText(this,"se ha producido un error al autenticar el usuario",Toast.LENGTH_LONG);
+    }
+
+    private void GoToHome(String email) {
+        Intent intent = new Intent(this,Home.class);
+        intent.putExtra("Correo",email);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/gobernanza/pruebalogin/Registro.java
@@ -0,0 +1,94 @@
+package com.gobernanza.pruebalogin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Registro extends AppCompatActivity implements View.OnClickListener {
+
+    EditText nombre, apellido, correo, password, CP;
+    TextView Return;
+    Button regis;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+        setTitle("REGISTRATE");
+
+        nombre = findViewById(R.id.editTextName);
+        apellido = findViewById(R.id.editTextLLastName);
+        correo = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById(R.id.editTextTextPassword);
+        CP = findViewById(R.id.editTextTextCP);
+        Return = findViewById(R.id.textViewReturnSign);
+        regis = findViewById(R.id.ButtonRegistro);
+
+        Return.setOnClickListener(this);
+        regis.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.textViewReturnSign:
+                GoToSignIn();
+                break;
+            case R.id.ButtonRegistro:
+                if (!nombre.getText().toString().isEmpty() && !apellido.getText().toString().isEmpty() && !correo.getText().toString().isEmpty() &&
+                !password.getText().toString().isEmpty() && ! CP.getText().toString().isEmpty()){
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(correo.getText().toString(), password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                GoToHome(task.getResult().getUser().getEmail() != null ? task.getResult().getUser().getEmail() : "");
+                            }else {
+                                showAlert();
+                            }
+                        }
+                    });
+
+                }
+                break;
+
+
+        }
+    }
+
+    private void showAlert() {
+        new AlertDialog.Builder(this)
+                .setTitle("Erro")
+                .setMessage("Se ha producido un error al autenticar el usuario")
+                .setPositiveButton("Aceptar", null)
+                .show();
+      //  Toast.makeText(this,"se ha producido un error al autenticar el usuario",Toast.LENGTH_LONG);
+    }
+
+    private void GoToHome(String emaail) {
+        Intent secundario = new Intent(this,Home.class);
+        startActivity(secundario);
+    }
+
+
+    private void GoToSignIn() {
+        Intent primario = new Intent(this,MainActivity.class);
+        startActivity(primario);
+    }
+}
